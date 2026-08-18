95c322cd67baf1524754604c51e3ae4b1d064594
Inbharasan <inbharasan06@gmail.com>
2026-08-18 15:22:18 +0530

    Add files via upload

---
+++ withparameter.java
@@ -0,0 +1,10 @@
+class Functions {
+int add(int a, int b) {
+return a + b;
+}
+public static void main(String[] args) {
+Functions f = new Functions();
+int result = f.add(10, 20);
+System.out.println(result);
+}
+}
